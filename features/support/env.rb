584a68d4e0be71682725126dddd923e242e7e52d
pikesley <github@orgraphone.org>
2014-02-02 22:56:36 +0000

    Tidy this shit up

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,10 +2,10 @@ require 'aruba/cucumber'
 require 'cucumber/rspec/doubles'
 require 'git'
 require 'fileutils'
-
 require 'coveralls'
-Coveralls.wear_merged!
 
 require 'badger/badger'
 
 $temp_repo = '/tmp/wow_repo'
+
+Coveralls.wear_merged!
